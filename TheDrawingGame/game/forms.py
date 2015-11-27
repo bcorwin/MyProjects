4c1294e568ed24d60d10c62f236fef03ad4f054b
Ben (work) <bscorwin@gmail.com>
2015-11-27 14:47:22 -0600

    Moved TheDrawingGame to own repo

--- TheDrawingGame/game/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class SaveImgForm(forms.Form):
-    img = forms.CharField(label='Image', max_length=10000000, widget=forms.HiddenInput(),
-            error_messages = {'required': 'Please draw something before submitting.'})
